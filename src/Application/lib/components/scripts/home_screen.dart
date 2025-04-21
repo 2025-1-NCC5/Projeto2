664d380d09c86412d55591da1dbdcba6b20868ed
Bruno Costa Dourado <brunoc.dourado10@gmail.com>
2025-04-21 16:50:26 -0300

    Vinheta

--- src/Application/lib/components/scripts/home_screen.dart
+++ src/Application/lib/components/scripts/home_screen.dart
@@ -13,7 +13,7 @@ class HomeScreen extends StatefulWidget {
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
-
+ 
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
--- src/Application/lib/components/scripts/tela_inicial.dart
+++ src/Application/lib/components/scripts/tela_inicial.dart
@@ -1,67 +1,39 @@
-import 'package:flutter/foundation.dart'; // kIsWeb
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:video_player/video_player.dart';
 import 'login_screen.dart';
 
 class TelaBoasVindas extends StatefulWidget {
   @override
-  _SplashVideoScreenState createState() => _SplashVideoScreenState();
+  _TelaBoasVindasState createState() => _TelaBoasVindasState();
 }
 
-class _SplashVideoScreenState extends State<TelaBoasVindas> {
-  late VideoPlayerController _controller;
-
+class _TelaBoasVindasState extends State<TelaBoasVindas> {
   @override
   void initState() {
     super.initState();
-
-    if (kIsWeb) {
-      _controller = VideoPlayerController.networkUrl(
-        Uri.parse('assets/web/vinheta.mp4'),
+    // Aguarda 4 segundos e vai para a próxima tela
+    Timer(Duration(seconds: 6), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => TelaLogin()),
       );
-    } else {
-      _controller = VideoPlayerController.asset('assets/web/vinheta.mp4');
-    }
-
-    _controller.initialize().then((_) {
-      _controller.setLooping(false);
-      _controller.play(); // ✅ Autoplay
-      setState(() {});
     });
-
-    _controller.addListener(() {
-      if (_controller.value.isInitialized &&
-          !_controller.value.isPlaying &&
-          _controller.value.position >= _controller.value.duration) {
-        Navigator.of(
-          context,
-        ).pushReplacement(MaterialPageRoute(builder: (context) => TelaLogin()));
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:
-          _controller.value.isInitialized
-              ? SizedBox.expand(
-                child: FittedBox(
-                  fit: BoxFit.cover,
-                  child: SizedBox(
-                    width: _controller.value.size.width,
-                    height: _controller.value.size.height,
-                    child: VideoPlayer(_controller),
-                  ),
-                ),
-              )
-              : Center(child: CircularProgressIndicator()),
+      backgroundColor: Colors.black, // opcional
+      body: Center(
+        child: SizedBox.expand(
+          child: FittedBox(
+            fit: BoxFit.cover, // ou .contain, .fitWidth etc
+            child: Image.asset(
+              'assets/web/vinheta.gif',
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
