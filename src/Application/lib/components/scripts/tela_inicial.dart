50a36214b078ee2ee0fb5649bcca51bf5b09cc2e
Bruno Costa Dourado <brunoc.dourado10@gmail.com>
2025-04-21 16:19:12 -0300

    Vinheta

--- src/Application/lib/components/scripts/tela_inicial.dart
+++ src/Application/lib/components/scripts/tela_inicial.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/foundation.dart'; // kIsWeb
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
-import 'login_screen.dart'; // substitua com sua tela principal
+import 'login_screen.dart';
 
 class TelaBoasVindas extends StatefulWidget {
   @override
@@ -13,19 +14,28 @@ class _SplashVideoScreenState extends State<TelaBoasVindas> {
   @override
   void initState() {
     super.initState();
-    _controller = VideoPlayerController.asset('assets\vinheta.mp4')
-      ..initialize().then((_) {
-        _controller.setLooping(false);
-        _controller.play(); // ✅ Autoplay
-        setState(() {});
-      });
 
-    // Ouve o término do vídeo
+    if (kIsWeb) {
+      _controller = VideoPlayerController.networkUrl(
+        Uri.parse('assets/web/vinheta.mp4'),
+      );
+    } else {
+      _controller = VideoPlayerController.asset('assets/web/vinheta.mp4');
+    }
+
+    _controller.initialize().then((_) {
+      _controller.setLooping(false);
+      _controller.play(); // ✅ Autoplay
+      setState(() {});
+    });
+
     _controller.addListener(() {
-      if (_controller.value.position >= _controller.value.duration) {
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => TelaLogin()),
-        );
+      if (_controller.value.isInitialized &&
+          !_controller.value.isPlaying &&
+          _controller.value.position >= _controller.value.duration) {
+        Navigator.of(
+          context,
+        ).pushReplacement(MaterialPageRoute(builder: (context) => TelaLogin()));
       }
     });
   }
@@ -39,18 +49,19 @@ class _SplashVideoScreenState extends State<TelaBoasVindas> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _controller.value.isInitialized
-          ? SizedBox.expand(
-              child: FittedBox(
-                fit: BoxFit.cover,
-                child: SizedBox(
-                  width: _controller.value.size.width,
-                  height: _controller.value.size.height,
-                  child: VideoPlayer(_controller),
+      body:
+          _controller.value.isInitialized
+              ? SizedBox.expand(
+                child: FittedBox(
+                  fit: BoxFit.cover,
+                  child: SizedBox(
+                    width: _controller.value.size.width,
+                    height: _controller.value.size.height,
+                    child: VideoPlayer(_controller),
+                  ),
                 ),
-              ),
-            )
-          : Center(child: CircularProgressIndicator()),
+              )
+              : Center(child: CircularProgressIndicator()),
     );
   }
 }
